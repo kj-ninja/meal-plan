3d4b6a8c425002475df1b81725c46d0fa3ff7d80
kj-ninja <jakubiak.krzy@gmail.com>
2020-07-08 11:10:05 +0200

    add mobile version for header with backdrop and sidedrawer

--- src/components/Header/Toolbar/Toolbar.js
+++ src/components/Header/Toolbar/Toolbar.js
@@ -3,14 +3,17 @@ import {Link} from "react-router-dom";
 import './Toolbar.scss';
 import NavigationItems from "../NavigationItems/NavigationItems";
 
-const Toolbar = () => {
+const Toolbar = (props) => {
     return (
         <header>
             <div className="header__container">
                 <Link to="/">
                     <h1><span>Meal</span> Plan</h1>
                 </Link>
-                <NavigationItems/>
+                <i className="fas fa-bars hamburger" onClick={props.showDrawer}/>
+                <nav>
+                    <NavigationItems/>
+                </nav>
             </div>
         </header>
     );
