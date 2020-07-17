af64aadaf8b975dd2f0c6bfb7e1a3bbf409de14f
kj-ninja <jakubiak.krzy@gmail.com>
2020-07-17 14:28:51 +0200

    meal-plan vool. 1 done, commit before deploy

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,18 +2,19 @@ import React, {useState} from 'react';
 import Lightbox from 'react-image-lightbox';
 import 'react-image-lightbox/style.css';
 import './About.scss';
+import useWindowWidth from "../../functions/customHooks/useWindowWidth";
 
 
 const recipesImages = [
-    require("../../assets/add-przepis.png"),
-    require("../../assets/przepisy.png")
+    require("../../assets/01.png"),
+    require("../../assets/04.png")
 ];
 const plansImages = [
-    require("../../assets/add-plan.png"),
-    require("../../assets/plany.png")
+    require("../../assets/05.png"),
+    require("../../assets/03.png")
 ];
 const pulpitImage = [
-    require("../../assets/pulpit.png")
+    require("../../assets/02.png")
 ];
 
 const About = () => {
@@ -22,6 +23,7 @@ const About = () => {
     const [isRecipesOpen, setIsRecipesOpen] = useState(false);
     const [isPlansOpen, setIsPlansOpen] = useState(false);
     const [isPulpitOpen, setIsPulpitOpen] = useState(false);
+    const width = useWindowWidth();
 
     return (
         <section className="about">
@@ -68,16 +70,16 @@ const About = () => {
                         Dodawaniem usuwanie oraz edytowanie przepisów, szybko i łatwo.
                         Dostęp do listy wszystkich zapisanych przepisów, kiedy tylko chcesz!
                     </div>
-                    <button type="button" onClick={()=>setIsRecipesOpen(true)}>Szczegóły >></button>
+                    {width < 1200 ? null : <button type="button" onClick={()=>setIsRecipesOpen(true)}>Szczegóły >></button>}
                 </div>
                 <div className="about__item">
                     <i className="fas fa-utensils"/>
                     <h2 className="about__title">Planuj swoje posiłki</h2>
                     <div className="about__description">
-                        W przypadku planów również możesz edytować, usuwać, edytować i przeglądać.
+                        W przypadku planów również możesz dodawać, usuwać, edytować i przeglądać.
                         Ponadto podczas tworzenia nowego planu korzystasz z wcześniej zapisanych przepisów.
                     </div>
-                    <button type="button" onClick={()=>setIsPlansOpen(true)}>Szczegóły >></button>
+                    {width < 1200 ? null : <button type="button" onClick={()=>setIsPlansOpen(true)}>Szczegóły >></button>}
                 </div>
                 <div className="about__item">
                     <i className="fas fa-heartbeat"/>
@@ -85,7 +87,7 @@ const About = () => {
                     <div className="about__description">
                         Mając dostęp do całej historii planów i przepisów, masz kontrolę nad tym, co jesz, dzięki czemu możesz być dużo zdrowszy!
                     </div>
-                    <button type="button" onClick={()=>setIsPulpitOpen(true)}>Szczegóły >></button>
+                    {width < 1200 ? null : <button type="button" onClick={()=>setIsPulpitOpen(true)}>Szczegóły >></button>}
                 </div>
             </div>
         </section>
